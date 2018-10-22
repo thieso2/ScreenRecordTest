ed0ef94dfff9eba8dd11ef904e9e2876d2d3a3f3
Thies C. Arntzen <thieso@gmail.com>
2018-10-22 12:58:58 +0200

    Initial Commit

---
+++ grab/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  grab
+//
+//  Created by Thies C. Arntzen on 22.10.18.
+//  Copyright © 2018 tmp8. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
